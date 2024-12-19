077abf3c26c114e0f2449cb4772eaaaf932e1e1a
krishnadev <krishnadevkichus@gmail.com>
2024-12-19 16:47:29 +0530

    feat: added project routers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors") // Import CORS
 const connectDB = require("./config/db")
 const authRoutes = require("./routes/authRoutes")
 const taskRoutes = require("./routes/taskRoutes")
+const projectRoutes = require("./routes/projectRoutes")
 
 dotenv.config()
 connectDB()
@@ -19,8 +20,11 @@ app.use(cors({
 app.use(express.json())
 
 // Routes
-app.use("/api/auth", authRoutes)
+app.use("/api/users", authRoutes)
 app.use("/api/tasks", taskRoutes)
+app.use("/api/projects" , projectRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
