283d2a26876c43ea0816b50b23354051089fa654
krishnadev <krishnadevkichus@gmail.com>
2024-12-20 10:56:24 +0530

    feat: include user information in login response for enhanced client-side handling

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -33,7 +33,7 @@ router.post('/login', async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        res.status(200).json({ token: generateToken(user._id) });
+        res.status(200).json({ token: generateToken(user._id) , user: user});
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
